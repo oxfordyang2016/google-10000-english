5abc15e95850e1204df0fb229d9faeb48235bae5
oxfordyang2016 <yang756260386@gamail.com>
2017-06-02 03:48:18 -0400

    allok

--- format.py
+++ format.py
@@ -2,7 +2,7 @@ with open('20k') as f:
     content = f.readlines()
 content=[x.strip() for x in content]
 for k in range(len(content)):
-    p=k*4
+    p=k*5
     open_string =str(p)+".{:<20} "+str(p+1)+".{:<20} "+str(p+2)+".{:<20} "+str(p+3)+".{:<20} "+str(p+4)+".{:<20}"
     #print open_string
     print(open_string.format(content[p],content[p+1],content[p+2],content[p+3],content[p+4]))
