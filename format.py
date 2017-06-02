45242eb422e61a8c6fbb9988a9144dc462ceabc2
oxfordyang2016 <yang756260386@gamail.com>
2017-06-02 04:45:29 -0400

    allok

--- format.py
+++ format.py
@@ -3,9 +3,9 @@ with open('20k') as f:
 content=[x.strip() for x in content]
 for k in range(len(content)):
     p=k*5
-    open_string =str(p)+".{:<17} "+str(p+1)+".{:<17} "+str(p+2)+".{:<17} "+str(p+3)+".{:<17} "+str(p+4)+".{:<17}"
+    open_string =str(p)+".{:<17} "+str(p+1)+".{:<17} "+str(p+2)+".{:<17} "+str(p+3)+".{:<17} "
     #print open_string
-    print(open_string.format(content[p],content[p+1],content[p+2],content[p+3],content[p+4]))
+    print(open_string.format(content[p],content[p+1],content[p+2],content[p+3]))
     #print(content[p],content[p+1],content[p+2])
     if p==19995:
         break
